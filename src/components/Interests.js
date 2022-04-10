03d91f941403c246ddeee7ff68cdbe68f1a19ca7
Jarod Wilson <jaroddwilson@gmail.com>
2022-04-10 17:20:53 -0400

    Completed assignment

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/user";
+import { ThemeContext } from "../context/theme";
 
-function Interests({ interests, theme }) {
+function Interests() {
+
+  const {user} = useContext(UserContext);
+  const {theme} = useContext(ThemeContext);
+
   return (
     <div>
       <h4>Interests</h4>
       <ul className={theme}>
-        {interests.map((interest) => (
+        {user.interests.map((interest) => (
           <li key={interest}>{interest}</li>
         ))}
       </ul>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { ThemeProvider } from "./context/theme";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ThemeProvider>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root"));
